7b5e2a8de9daf498c4673f5ac9d15b1a495efc79
Lythmass <lythmass17@gmail.com>
2022-04-18 16:05:19 +0400

    Initial Commit

---
+++ src/Components/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Welcome(props) {
+
+     return (
+          <div className = "welcome">
+               <h1>Quizzical</h1>
+               <h3>Test your general knowledge on this flamboyant quiz!</h3>
+               <button>Start Quiz</button>
+          </div>
+     )
+}
